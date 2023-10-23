9b6127e77d49804fac0717f04ed9adff580659f3
DevKiD <duynamschlitz@gmail.com>
2023-10-23 17:31:35 +0200

    Create Class: connect.cpp, disconnect.cpp. Add other function to ADB-Express Core Free Edition Version 1.0 Alpha Beta: Data Transfer Protocol Wine Taste

--- main.cpp
+++ main.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
 #include <string>
-#include <stdexcept>
 #include <random>
 
-#include <disconnect.cpp>
+#include "disconnect.cpp"
+#include "connect.cpp"
 
 using namespace std;
 
 int adb_install() {
     try {
-        int result = system("adb");
+        int result = system("adb version");
         if (result != 0) {
-            cout << "ADB is not installed. Please install ADB." << endl;
-            cout << "Install ADB..." << endl;
+            std::cout << "ADB is not installed. Please install ADB." << std::endl;
+
             // Check which Operating System is running
-            if (system("uname -a | grep -i Darwin")) {
-                // Mac OS
-                system("brew cask install android-platform-tools");
-            } else if (system("uname -a | grep -i Linux")) {
-                // Linux
-                system("sudo apt-get install android-tools-adb");
-            } else if (system("uname -a | grep -i Windows")) {
-                // Windows
-                system("choco install adb");
-            } else {
+#ifdef _WIN32
+            std::cout << "This is a Windows operating system." << std::endl;
+            // You may put Windows-specific installation commands here.
+#elif __APPLE__
+            std::cout << "This is an Apple macOS operating system." << std::endl;
+                system("brew install adb"); // Install ADB on macOS using Homebrew.
+#elif __linux__
+                std::cout << "This is a Linux operating system." << std::endl;
+                system("sudo apt install adb"); // Install ADB on Linux using apt.
+#elif __unix__
+                std::cout << "This is a Unix-based operating system." << std::endl;
+                system("sudo apt install adb"); // Install ADB on Unix-based systems using apt.
+#else
                 throw std::runtime_error("ADB installation failed.");
-            }
-            throw std::runtime_error("ADB installation failed.");
+#endif
+        } else {
+            return 0;
         }
     } catch (const std::exception &e) {
         std::cerr << e.what() << '\n';
@@ -35,13 +39,22 @@ int adb_install() {
     return 0;
 }
 
+int add_devices(string ip) {
+    devices.push_back(ip);
+    return 0;
+}
+
 int main() {
 
-    if (!adb_install()) {
+/*    if (!adb_install()) {
         cout << "ADB is not installed. Please install ADB." << endl;
         return 1;
     }
-
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif */
     vector<string> devices;
     char buffer[128];
     FILE *pipe = popen("adb devices", "r");
@@ -53,6 +66,8 @@ int main() {
     cout << "Restart the ADB services..." << endl;
     system("adb kill-server && adb start-server");
 
+    cout << "Welcome to ADB-Express Core Free Edition Version 1.0 Alpha Beta: Data Transfer Protocol Wine Taste" << endl;
+
     while (true) {
 
         int mode;
@@ -60,15 +75,16 @@ int main() {
         cout << "1 for connect mode" << endl;
         cout << "2 disconnect" << endl;
         cout << "3 Open App" << endl;
+        cout << "3.1 Get App"s << endl;
         cout << "4 Close App" << endl;
         cout << "5 Open Script" << endl;
         cout << "6 Handle manuel" << endl;
         cout << "7 List devices" << endl;
         cout << "8 exit" << endl;
+        cout << "9 Advance" << endl;
         cout << "Enter mode (): ";
-        cin >> mode;
 
-        if (mode == 0) {
+        if (cin.get() ==0) {
             string deviceIP;
             cout << "Enter the IP:PORT to pair the devices: ";
             cin >> deviceIP;
@@ -82,49 +98,16 @@ int main() {
             }
 
             cout << "\nPaired to " << deviceIP << " was successfully." << endl;
-        } else if (mode == 1) {
-            cout << "0 via WiFi" << endl;
-            cout << "1 via USB" << endl;
-            cout << "Enter mode (): ";
-            cin >> mode;
-
-            if (mode == 0) {
-                string device_ip;
-                cout << "Enter the IP address of your Android device: ";
-                cin >> device_ip;
-
-                try {
-                    system(("adb connect " + device_ip).c_str());
-
-                } catch (const std::exception &e) {
-                    std::cerr << e.what() << '\n';
-                    return 1;
-                }
-
-                devices.push_back(device_ip);
-
-                cout << "Connected to " << device_ip << endl;
-            } else if (mode == 1) {
-                try {
-                    system("adb usb");
-
-                } catch (const std::exception &e) {
-                    std::cerr << e.what() << '\n';
-                    return 1;
-                }
-
-                cout << "Connected via USB" << endl;
-            } else {
-                cout << "Don't exist";
-            }
-
-        } else if (mode == 2) {
+        } else if (cin.get() ==1) {
+            connect();
+        } else if (cin.get() ==2) {
             cout << "Enter the index you want disconnect (all, 0, 1...): ";
-            disconnect(cin.get());
+            string i;
+            cin >> i;
+            disconnect(i);
         } else {
             cout << "Don't exist";
         }
     }
-
     return 0;
 }
